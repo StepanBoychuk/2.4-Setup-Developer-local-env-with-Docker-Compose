a28e7a7111a33f889389def9273741d763a2a6e6
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-12-05 16:45:57 +0200

    Added .env file and node.js script

---
+++ 2.4-setup-developer-local-env.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv'); 
+const pg = require('pg');
+const redis = require('redis');
+const amqp = require('amqplib');
+
+dotenv.config();
+
+const db_client = new pg.Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+});
+
+const redis_client =  redis.createClient({
+    url: process.env.REDIS_URL
+}).on('error', err => console.log('Redis Client Error', err))
+
+const connectToRabbitMQ = async () => {
+    try {
+        const connection = await amqp.connect(`amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`);
+        const channel= await connection.createChannel();
+        await channel.assertQueue('test-queue');
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+db_client.connect();
+redis_client.connect();
+connectToRabbitMQ();
